4d3df9175c3862749c15d158d617dd2c5b45da31
Mads Kristensen <post@madskristensen.net>
2015-10-22 22:18:50 -0700

    Chocolatey updates

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 using PackageInstaller;
 
-[assembly: AssemblyTitle("Package Installer")]
+[assembly: AssemblyTitle(Constants.VSIX_NAME)]
 [assembly: AssemblyDescription("Makes it easier, faster and more convenient than ever to install Bower and npm packages to any project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mads Kristensen")]
-[assembly: AssemblyProduct("Package Installer")]
+[assembly: AssemblyProduct(Constants.VSIX_NAME)]
 [assembly: AssemblyCopyright("Mads Kristensen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
